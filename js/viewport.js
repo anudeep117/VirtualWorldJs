e08440a0a106a316c11816a813151afbd7100f6c
anudeep <anudeep.reddy1725@gmail.com>
2025-04-06 19:58:25 -0700

    added zoom, pan

--- js/viewport.js
+++ js/viewport.js
@@ -4,15 +4,78 @@ class Viewport {
         this.ctx = canvas.getContext("2d");
 
         this.zoom = 1;
+        this.center = new Point(canvas.width / 2, canvas.height / 2);
+        this.offset = scale(this.center, -1)
+
+        this.drag = {
+            start: new Point(0, 0),
+            end: new Point(0, 0),
+            offset: new Point(0, 0),
+            active: false,
+        };
 
         this.#addEventListeners();
     }
 
+    reset() {
+        this.ctx.restore();
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.save();
+        this.ctx.translate(this.center.x, this.center.y);
+        this.ctx.scale(1 / this.zoom, 1 / this.zoom);
+        const offset = this.getOffset();
+        ctx.translate(offset.x, offset.y);
+    }
+
+    getMouse(evt, subtractOffset = false) {
+        const p = new Point(
+            (evt.offsetX - this.center.x) * this.zoom - this.offset.x,
+            (evt.offsetY - this.center.y) * this.zoom - this.offset.y
+        );
+        return subtractOffset ? subtract(p, this.drag.offset) : p;
+    }
+
+    getOffset() {
+        return add(this.offset, this.drag.offset);
+    }
+
     #addEventListeners() {
         this.canvas.addEventListener("mousewheel", this.#handleMouseWheel.bind(this));
+        this.canvas.addEventListener("mousedown", this.#handleMouseDown.bind(this));
+        this.canvas.addEventListener("mousemove", this.#handleMouseMove.bind(this));
+        this.canvas.addEventListener("mouseup", this.#handleMouseUp.bind(this));
     }
 
     #handleMouseWheel(evt) {
-        console.log(evt.deltaY);
+        const dir = Math.sign(evt.deltaY);
+        const step = 0.1;
+        this.zoom += dir * step;
+        this.zoom = Math.max(1, Math.min(this.zoom, 5))
+    }
+
+    #handleMouseDown(evt) {
+        if (evt.button == 1) { // middle button
+            this.drag.start = this.getMouse(evt);
+            this.drag.active = true;
+        }
+    }
+
+    #handleMouseMove(evt) {
+        if (this.drag.active) {
+            this.drag.end = this.getMouse(evt);
+            this.drag.offset = subtract(this.drag.end, this.drag.start);
+        }
+    }
+
+    #handleMouseUp(evt) {
+        if (this.drag.active) {
+            this.offset = this.getOffset();
+            this.drag = {
+                start: new Point(0, 0),
+                end: new Point(0, 0),
+                offset: new Point(0, 0),
+                active: false,
+            };
+        }
     }
 }
